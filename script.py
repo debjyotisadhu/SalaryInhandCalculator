093c058672311d52a7bfd4b196ed58332d495cc8
debjyotisadhu <debjyotisadhu@gmail.com>
2026-06-09 11:08:00 +0530

    Update script.py

--- script.py
+++ script.py
@@ -64,10 +64,8 @@ annual_basic = st.number_input("Basic Salary (₹ / year)", min_value=0.0, value
 annual_allowance = st.number_input("Allowances (HRA + Other) (₹ / year)", min_value=0.0, value=200000.0, step=1000.0)
 annual_epf = st.number_input("Employee EPF Contribution (₹ / year)", min_value=0.0, value=annual_basic*0.12, step=500.0)
 # ON / OFF switch
-feature_on = st.toggle("Enable Employer NPS Benefit (14% of Basic)")
-annual_nps = 0.14*annual_basic*feature_on
 nps_rate = st.slider("Employer NPS Contribution (%)", min_value=0.0, max_value=14.0, value=0.0, step=1.0)
-annual_nps = nps_rate*annual_basic
+annual_nps = nps_rate*annual_basic/100.0
 
 feature_on = st.toggle("Add Gratuity into CTC")
 
